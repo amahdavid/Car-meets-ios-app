eadc29b8f5ec3c081f4252412942221079585592
amahdavid <amahdavid34@gmail.com>
2024-04-29 23:47:46 -0700

    Bottom Navbar Implemented

--- Car-meets/ContentView.swift
+++ Car-meets/ContentView.swift
@@ -15,14 +15,45 @@ struct ContentView: View {
         UITabBar.appearance().isHidden = true
     }
     
+    // used for matching geometrical effects
+    @Namespace var animation
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        TabView(selection: $currentTab) {
+            Text("Home View").frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/, maxHeight: .infinity).background().tag(Tab.Home)
+            Text("Search View").frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/, maxHeight: .infinity).tag(Tab.Search)
+            Text("Notifications View").frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/, maxHeight: .infinity).tag(Tab.Notifications)
+            Text("Bookmark View").frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/, maxHeight: .infinity).tag(Tab.Bookmark)
+            Text("Profile View").frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/, maxHeight: .infinity).tag(Tab.Profile)
         }
-        .padding()
+        .overlay(HStack(spacing: 0){
+            ForEach(Tab.allCases, id: \.rawValue){
+                tab in TabButton(tab: tab)
+            }
+            .padding(.vertical)
+            .padding(.bottom, getSafeArea().bottom == 0 ? 5 : (getSafeArea().bottom - 15))
+            .background(Color("kSecondary"))
+        },
+                 alignment: .bottom
+        ).ignoresSafeArea(.all, edges: .bottom)
+    }
+    
+    func TabButton(tab: Tab) -> some View {
+        GeometryReader {proxy in Button(action: {
+            withAnimation(.spring()) {
+                currentTab = tab
+            }
+        }, label: {
+            VStack(spacing: 0) {
+                Image(systemName: currentTab == tab ? tab.rawValue + ".fill": tab.rawValue).resizable().foregroundColor(Color("kPrimary")).aspectRatio(contentMode: .fit).frame(width: 25, height: 25).frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/).background(ZStack{
+                    if currentTab == tab {
+                        MaterialEffect(style: .light).clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/).matchedGeometryEffect(id: "Tab", in: animation)
+                        
+                        Text(tab.TabName).foregroundColor(.black).font(.footnote).padding(.top, 50)
+                    }
+                }).contentShape(Rectangle()).offset(y: currentTab == tab ? -15 : 0)
+            }
+        })}.frame(height: 25)
     }
 }
 
@@ -34,7 +65,7 @@ enum Tab: String, CaseIterable {
     case Home = "house"
     case Search = "magnifyingglass.circle"
     case Notifications = "bell"
-    case Saved = "saved"
+    case Bookmark = "bookmark.circle"
     case Profile = "person"
     
     var TabName: String {
@@ -45,10 +76,38 @@ enum Tab: String, CaseIterable {
             return "Search"
         case .Notifications:
             return "Notifications"
-        case .Saved:
-            return "Saved"
+        case .Bookmark:
+            return "Bookmark"
         case .Profile:
             return "Profile"
         }
+    }
+}
+
+extension View {
+    func getSafeArea() -> UIEdgeInsets {
+        guard let screen = UIApplication.shared.connectedScenes.first as?
+                UIWindowScene else {
+            return .zero
+        }
+        guard let safeArea = screen.windows.first?.safeAreaInsets else {
+            return .zero
+        }
+        
+        return safeArea
+    }
+}
+
+struct MaterialEffect: UIViewRepresentable {
+    var style: UIBlurEffect.Style
+    
+    func makeUIView(context: Context) -> UIVisualEffectView {
+        let view = UIVisualEffectView(effect: UIBlurEffect(style: style))
+        
+        return view
+    }
+    
+    func updateUIView(_ uiView: UIVisualEffectView, context: Context) {
+        
     }
 }
