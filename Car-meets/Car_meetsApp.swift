4265d784652fb856680a74491aefc56abff249d4
amahdavid <amahdavid34@gmail.com>
2024-04-29 22:12:25 -0700

    Initial Commit

---
+++ Car-meets/Car_meetsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Car_meetsApp.swift
+//  Car-meets
+//
+//  Created by David Chika Amah-Nnachi on 2024-04-29.
+//
+
+import SwiftUI
+
+@main
+struct Car_meetsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Car-meetsUITests/Car_meetsUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Car_meetsUITestsLaunchTests.swift
+//  Car-meetsUITests
+//
+//  Created by David Chika Amah-Nnachi on 2024-04-29.
+//
+
+import XCTest
+
+final class Car_meetsUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
