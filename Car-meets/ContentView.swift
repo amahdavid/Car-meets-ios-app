e9c3cecfd9182f602761458bee0b2e656e7d8617
amahdavid <amahdavid34@gmail.com>
2024-04-29 22:27:48 -0700

    Publishing repo, added images, starting navbar

--- Car-meets/ContentView.swift
+++ Car-meets/ContentView.swift
@@ -8,6 +8,13 @@
 import SwiftUI
 
 struct ContentView: View {
+    // whenever the app is open it redirects to the home page
+    @State var currentTab: Tab = .Home
+    
+    init() {
+        UITabBar.appearance().isHidden = true
+    }
+    
     var body: some View {
         VStack {
             Image(systemName: "globe")
@@ -21,4 +28,27 @@ struct ContentView: View {
 
 #Preview {
     ContentView()
+}
+
+enum Tab: String, CaseIterable {
+    case Home = "house"
+    case Search = "magnifyingglass.circle"
+    case Notifications = "bell"
+    case Saved = "saved"
+    case Profile = "person"
+    
+    var TabName: String {
+        switch self {
+        case .Home:
+            return "Home"
+        case .Search:
+            return "Search"
+        case .Notifications:
+            return "Notifications"
+        case .Saved:
+            return "Saved"
+        case .Profile:
+            return "Profile"
+        }
+    }
 }
